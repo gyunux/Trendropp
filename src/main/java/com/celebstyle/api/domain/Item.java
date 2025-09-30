cc6438f8336aa6bb8d13ac3a3def180e65ded00b
gyunux <67477254+gyunux@users.noreply.github.com>
2025-09-30 09:42:59 +0900

    feat: Item Entity

---
+++ src/main/java/com/celebstyle/api/domain/Item.java
@@ -0,0 +1,39 @@
+package com.celebstyle.api.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,length = 2048)
+    private String imageUrl;
+
+    @Column(nullable = false,length = 2048)
+    private String productUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+}
