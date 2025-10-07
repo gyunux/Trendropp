1b9119ec9bdc16b0b65e8eb970b7142e268b8a2b
gyunux <67477254+gyunux@users.noreply.github.com>
2025-10-07 18:11:50 +0900

    feat: Article For Outfit

--- src/main/java/com/celebstyle/api/article/ArticleAdminController.java
+++ src/main/java/com/celebstyle/api/article/ArticleAdminController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,9 +15,13 @@ public class ArticleAdminController {
     private final ArticleService articleService;
 
     @GetMapping
-    public String getArticleDashboard(Model model){
+    public String getArticleDashboard(Model model,@RequestParam(required = false) String mode){
         List<ArticleAdminView> articles = articleService.findAllForAdminView();
         model.addAttribute("articles",articles);
+
+        // "mode" 파라미터가 "select"와 일치하는지 여부를 boolean으로 전달
+        model.addAttribute("isSelectionMode", "select".equals(mode));
+
         return "admin/articles";
     }
 }
