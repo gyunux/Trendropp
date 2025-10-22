dfb1f53ca9cb622286e90e42d865e33a8a63352c
gyunux <67477254+gyunux@users.noreply.github.com>
2025-10-22 14:38:30 +0900

    feat: AWS S3Client

---
+++ src/main/java/com/celebstyle/api/celeb/S3UploadService.java
@@ -0,0 +1,34 @@
+package com.celebstyle.api.celeb;
+
+import java.io.IOException;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3UploadService {
+    private final S3Client s3Client;
+    private final String bucketName = "celebstyletest-storage";
+
+    @Autowired
+    public S3UploadService(S3Client s3Client){
+        this.s3Client = s3Client;
+    }
+
+    public String profileUpload(MultipartFile file,String directory) throws IOException{
+        String uniqueFileName = directory + "/"  + UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(uniqueFileName)
+                .contentType(file.getContentType())
+                .build();
+        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+        return "https://" + bucketName + ".s3.ap-northeast-2.amazonaws.com/" + uniqueFileName;
+    }
+}
--- src/main/java/com/celebstyle/api/config/AppConfig.java
+++ src/main/java/com/celebstyle/api/config/AppConfig.java
@@ -5,6 +5,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AppConfig {
@@ -15,4 +17,11 @@ public class AppConfig {
                 .setConnectTimeout(Duration.ofSeconds(10)) // 연결 시도 시간 10초
                 .setReadTimeout(Duration.ofSeconds(30))    // 응답 대기 시간 30초
                 .build();    }
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2) // 서울 리전
+                .build();
+    }
 }
