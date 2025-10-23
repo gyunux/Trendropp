5b5d99ee134e9b6277c0b9d7bf4ef327d98c8190
gyunux <67477254+gyunux@users.noreply.github.com>
2025-10-23 21:59:49 +0900

    feat: URL -> S3 Storage Upload

--- src/main/java/com/celebstyle/api/common/S3UploadService.java
+++ src/main/java/com/celebstyle/api/common/S3UploadService.java
@@ -1,7 +1,10 @@
 package com.celebstyle.api.common;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.UUID;
+import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,6 +13,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service
+@Value
 public class S3UploadService {
     private final S3Client s3Client;
     private final String bucketName = "celebstyletest-storage";
@@ -30,5 +34,24 @@ public class S3UploadService {
         s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
 
         return "https://" + bucketName + ".s3.ap-northeast-2.amazonaws.com/" + uniqueFileName;
+    }
+
+    public String uploadFromUrl(String imageUrl, String directory) throws IOException {
+        String uniqueFileName = directory + "/" + UUID.randomUUID().toString() + ".jpg";
+
+        URL url = new URL(imageUrl);
+        try (InputStream in = url.openStream()) {
+
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(uniqueFileName)
+                    .contentType("image/jpeg") // 이미지 타입 지정
+                    .build();
+            byte[] imagesBytes = in.readAllBytes();
+            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(imagesBytes));
+
+            // 5. 업로드된 파일의 최종 S3 URL을 반환합니다.
+            return "https://" + bucketName + ".s3.ap-northeast-2.amazonaws.com/" + uniqueFileName;
+        }
     }
 }
