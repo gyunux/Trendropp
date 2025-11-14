0cc1e2f9e9dca1286f5a75bd6ba6cff52d907439
gyunux <67477254+gyunux@users.noreply.github.com>
2025-11-14 16:47:16 +0900

    refactor: List<String> imageUrls -> ArticleImage Entity로 분리

---
+++ src/main/java/com/celebstyle/api/article/ArticleImage.java
@@ -0,0 +1,35 @@
+package com.celebstyle.api.article;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class ArticleImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @ManyToOne
+    private Article article;
+
+    protected void setArticle(Article article) {
+        this.article = article;
+    }
+
+    public ArticleImage(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
