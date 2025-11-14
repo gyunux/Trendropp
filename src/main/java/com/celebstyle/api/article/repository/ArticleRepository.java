2e2e43c01eed43146e4d46add97c462ae3101a7b
gyunux <67477254+gyunux@users.noreply.github.com>
2025-11-14 16:45:06 +0900

    chore: 패키지 이동

--- src/main/java/com/celebstyle/api/article/repository/ArticleRepository.java
+++ src/main/java/com/celebstyle/api/article/repository/ArticleRepository.java
@@ -1,12 +1,16 @@
-package com.celebstyle.api.article;
+package com.celebstyle.api.article.repository;
 
+import com.celebstyle.api.article.Article;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArticleRepository extends JpaRepository<Article,Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     boolean existsByArticleUrl(String articleUrl);
+
+    //    @Query("SELECT * FROM article a")
     List<Article> findAllByOrderByArticleDateDesc();
+
 }
