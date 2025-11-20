8e632ea6ff3c898cccd740ab7ae260daf31babab
gyunux <67477254+gyunux@users.noreply.github.com>
2025-11-20 10:07:23 +0900

    test: Celeb CRUD 테스트

--- src/test/java/com/celebstyle/api/celeb/CelebRepositoryTest.java
+++ src/test/java/com/celebstyle/api/celeb/CelebRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.celebstyle.api.article.repository.ArticleRepositoryImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -26,26 +27,81 @@ public class CelebRepositoryTest {
     ArticleRepositoryImpl articleRepositoryImpl;
 
     private Celeb validCeleb;
+    private Celeb preSavedCeleb;
 
     @BeforeEach
     void setUp() {
         validCeleb = Celeb.builder()
-                .nameKo("테스트 셀럽")
-                .nameEn("Test Celeb")
-                .profileImageUrl("https://test.com.profile.jpg")
-                .instagramName("test_insta")
+                .nameKo("테스트 셀럽1")
+                .nameEn("Test Celeb1")
+                .profileImageUrl("https://test1.com.profile.jpg")
+                .instagramName("test1_insta")
                 .build();
+
+        preSavedCeleb = Celeb.builder()
+                .nameKo("테스트 셀럽2")
+                .nameEn("Test Celeb2")
+                .profileImageUrl("https://test2.com.profile.jpg")
+                .instagramName("test2_insta")
+                .build();
+        preSavedCeleb = celebRepository.save(preSavedCeleb);
     }
 
     @Test
-    void createCelebTest() {
+    @DisplayName("셀럽 생성 테스트")
+    void celebCreateTest() {
         Celeb savedCeleb = celebRepository.save(validCeleb);
 
         assertThat(savedCeleb).isNotNull();
-        assertThat(savedCeleb.getNameKo()).isEqualTo("테스트 셀럽");
-        assertThat(savedCeleb.getNameEn()).isEqualTo("Test Celeb");
-        assertThat(savedCeleb.getProfileImageUrl()).isEqualTo("https://test.com.profile.jpg");
-        assertThat(savedCeleb.getInstagramName()).isEqualTo("test_insta");
+        assertThat(savedCeleb.getNameKo()).isEqualTo("테스트 셀럽1");
+        assertThat(savedCeleb.getNameEn()).isEqualTo("Test Celeb1");
+        assertThat(savedCeleb.getProfileImageUrl()).isEqualTo("https://test1.com.profile.jpg");
+        assertThat(savedCeleb.getInstagramName()).isEqualTo("test1_insta");
+    }
 
+    @Test
+    @DisplayName("셀럽 저장 테스트")
+    void celebSaveTest() {
+        Celeb findedCeleb = celebRepository.findById(1L).orElse(null);
+
+        assertThat(findedCeleb).isNotNull();
+        assertThat(findedCeleb.getNameKo()).isEqualTo("테스트 셀럽2");
+        assertThat(findedCeleb.getNameEn()).isEqualTo("Test Celeb2");
+        assertThat(findedCeleb.getProfileImageUrl()).isEqualTo("https://test2.com.profile.jpg");
+        assertThat(findedCeleb.getInstagramName()).isEqualTo("test2_insta");
+    }
+
+    @Test
+    @DisplayName("셀럽 수정 테스트")
+    void celebUpdateTest() {
+        preSavedCeleb.setNameEn("Test Two Celeb");
+        String newNameEn = "Test Two Celeb Updated";
+        String newNameKo = "테스트 셀럽 2 수정됨";
+
+        preSavedCeleb.setNameEn(newNameEn);
+        preSavedCeleb.setNameKo(newNameKo);
+
+        Celeb updatedCeleb = celebRepository.save(preSavedCeleb);
+
+        Long celebId = updatedCeleb.getId();
+
+        Celeb foundCeleb = celebRepository.findById(celebId).orElse(null);
+
+        assertThat(foundCeleb).isNotNull();
+
+        assertThat(foundCeleb.getNameEn()).isEqualTo(newNameEn);
+        assertThat(foundCeleb.getNameKo()).isEqualTo(newNameKo);
+
+        assertThat(foundCeleb.getInstagramName()).isEqualTo("test2_insta");
+    }
+
+    @Test
+    @DisplayName("셀럽 삭제 테스트")
+    void celebDeleteTest() {
+        celebRepository.delete(preSavedCeleb);
+
+        Celeb deletedCeleb = celebRepository.findById(2L).orElse(null);
+
+        assertThat(deletedCeleb).isNull();
     }
 }
