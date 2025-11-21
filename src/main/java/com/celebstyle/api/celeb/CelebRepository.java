a07671ec623fd48d77fce780dc0de9d0b6681b3a
gyunux <67477254+gyunux@users.noreply.github.com>
2025-11-21 17:31:15 +0900

    feat: 이름으로 엔티티 조회

--- src/main/java/com/celebstyle/api/celeb/CelebRepository.java
+++ src/main/java/com/celebstyle/api/celeb/CelebRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CelebRepository extends JpaRepository<Celeb,Long> {
+public interface CelebRepository extends JpaRepository<Celeb, Long> {
+    Celeb findByNameKo(String nameKo);
 }
