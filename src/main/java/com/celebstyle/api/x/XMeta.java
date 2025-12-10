f2c284e3753ce164d4f3935412009a851938cfdf
gyunux <67477254+gyunux@users.noreply.github.com>
2025-12-10 14:31:08 +0900

    remove: X 관련 코드 삭제

--- src/main/java/com/celebstyle/api/x/XMeta.java
+++
@@ -1,9 +0,0 @@
-package com.celebstyle.api.x;
-
-public record XMeta(
-        int result_count,
-        String newest_id,
-        String oldest_id,
-        String next_token
-) {
-}
--- src/main/java/com/celebstyle/api/x/XTweet.java
+++
@@ -1,8 +0,0 @@
-package com.celebstyle.api.x;
-
-public record XTweet(
-        String id,
-        String edit_history_tweet_ids,
-        String text
-) {
-}
